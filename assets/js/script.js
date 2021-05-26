7090da1e2c27cf9d1442a6a2521e741311153cab
Fiona-T <fitracey@gmail.com>
2021-05-26 09:43:25 +0000

    set up structure of functions for the game

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// run the game
+function runGame() {
+
+}
+
+// check the answer
+function checkAnswer() {
+
+}
+
+// calculte answer
+function calculteCorrectAnswer() {
+    
+}
+
+// increment the correct score
+function incrementScore() {
+    
+}
+
+// increment the wrong answers
+function incrementWrongAnswer() {
+    
+}
+
+// display the questions
+function displayAdditionQuestion() {
+    
+}
+
+// display the questions
+function displaySubtractQuestion() {
+    
+}
+
+// display the questions
+function displayMultiplyQuestion() {
+    
+}
